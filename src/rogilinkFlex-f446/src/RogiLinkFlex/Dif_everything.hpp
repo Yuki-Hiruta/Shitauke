4900de2e65d889ad380811427f09cd0c02ded5b3
Yuki-Hiruta <yuki060328@keio.jp>
2024-11-07 16:49:06 +0900

    ｈくえｎclwnfcolkmwq

--- src/rogilinkFlex-f446/src/RogiLinkFlex/Dif_everything.hpp
+++ src/rogilinkFlex-f446/src/RogiLinkFlex/Dif_everything.hpp
@@ -1,17 +1,17 @@
 #include <mbed.h>
 
-const PinName interrupt_in_pin_e1 = PB_1;
-const PinName interrupt_in_pin_e2 = PB_2;
-const PinName interrupt_in_pin_e3 = PB_3;
+const PinName interrupt_in_pin_e1 = PA_15;
+const PinName interrupt_in_pin_e2 = PB_2;   //適当
+const PinName interrupt_in_pin_e3 = PB_3;   //適当
 
-const PinName digital_in_pin_e1 = PA_1;
-const PinName digital_in_pin_e2 = PA_2;
-const PinName digital_in_pin_e3 = PA_3;
+const PinName digital_in_pin_e1 = PC_12;
+const PinName digital_in_pin_e2 = PA_2;     //適当
+const PinName digital_in_pin_e3 = PA_3;     //適当
 
 const PinName dir_pin_m1 = PA_9;
-const PinName dir_pin_m2 = PB_2;
-const PinName dir_pin_m3 = PB_1;
+const PinName dir_pin_m2 = PB_2;    //適当
+const PinName dir_pin_m3 = PB_1;    //適当
 
-const PinName pwm_pin_m1 = PB_13;
-const PinName pwm_pin_m2 = PB_14;
-const PinName pwm_pin_m3 = PB_15;
+const PinName pwm_pin_m1 = PB_8;
+const PinName pwm_pin_m2 = PB_14;   //適当
+const PinName pwm_pin_m3 = PB_15;   //適当
